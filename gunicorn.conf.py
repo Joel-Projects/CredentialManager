ba1506eaee280711c4ff2f7c0f544ad18a5f4906
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2021-10-02 19:41:16 -0500

    change tmp dir to /dev/shm

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,7 @@ psycogreen.gevent.patch_psycopg()
 accesslog = "-"
 bind = ":5000"
 worker_class = "gevent"
-timeout = 900
+# timeout = 900
+worker_tmp_dir = '/dev/shm'
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "app:create_app()"
